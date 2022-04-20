8bb4eddff5aa72d0a4f7e1bd6ccc213d9987484c
azalfadheel <104054081+azalfadheel@users.noreply.github.com>
2022-04-20 08:49:37 +0300

    Add files via upload

---
+++ MySqliteDBRE.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using SQLite;
+using System.IO;
+using System.Collections.Generic;
+
+namespace XA_Mid2_Lab_Re
+{
+    class MySqliteDBRE
+    {
+        //database path
+        private string dbPath = Path.Combine(
+                System.Environment.GetFolderPath(System.Environment.SpecialFolder.Personal), "MyContacts.db3");
+        public MySqliteDBRE()
+        {
+            //Creating database, if it doesn't already exist 
+            if (!File.Exists(dbPath))
+            {
+                var db = new SQLiteConnection(dbPath);
+                db.CreateTable<Contacts>();
+            }
+        }
+        //Insert data into DB  
+        public void Insert(Contacts contact)
+        {
+            var db = new SQLiteConnection(dbPath);
+            db.Insert(contact);
+        }
+        //Update data in DB   
+        public void Update(Contacts contact)
+        {
+            var db = new SQLiteConnection(dbPath);          
+            db.Update(contact);
+        }
+        // Return True if data is found othewies return false
+        public bool Check(string name)
+        {
+            var db = new SQLiteConnection(dbPath);
+            var table = db.Table<Contacts>().Where(i => i.Name == name).FirstOrDefault();
+            if (table == null)
+                return true;
+            return false;
+        }
+        //Return Table from DB  
+        public Contacts GetContact(string name, string mobile)
+        {
+            var db = new SQLiteConnection(dbPath);
+            return db.Table<Contacts>().Where(i => i.Name == name && i.Mobile == mobile).FirstOrDefault(); ;                      
+        }
+
+        public Contacts GetContactById(int id)
+        {
+            var db = new SQLiteConnection(dbPath);
+            return db.Table<Contacts>().Where(i => i.Id == id).FirstOrDefault(); ;
+        }
+        public List<Contacts> GetAllUsers()
+        {
+            var db = new SQLiteConnection(dbPath);
+            return db.Table<Contacts>().ToList();
+        }
+
+        [Table("Contacts")]
+        public class Contacts
+        {
+            [PrimaryKey, AutoIncrement, Column("_id")]
+            public int Id { get; set; }
+            public string Name { get; set; }
+            public string Mobile { get; set; }
+        }
+    }
+}
---
+++ ShowActivity.cs
@@ -0,0 +1,47 @@
+﻿
+using Android.App;
+using Android.OS;
+using Android.Widget;
+using XA_Mid2_Lab_Re;
+
+namespace XA1_Mid2_Lab
+{
+    [Activity(Label = "Show_Activity")]
+    public class ShowActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            SetContentView(Resource.Layout.activity_show);
+
+            var show2 = FindViewById<TextView>(Resource.Id.show);
+            
+            var showall2 = FindViewById<Button>(Resource.Id.showall);
+
+
+
+
+
+
+
+
+
+
+
+            showall2.Click += delegate
+            {
+
+                var sq = new MySqliteDBRE();
+                var Contacts = sq.GetAllUsers();
+
+                string s = "";
+                foreach (var Contact in Contacts)
+                {
+                    s += Contact.Id + "    " + Contact.Name + "   " + Contact.Mobile + "\n";
+                }
+
+                show2.Text = s.ToString();
+            };
+        }
+    }
+}
